480445fa6ec6d7103ba2f32c65d5596e06c1b6c6
anoukruhaak <anoukruhaak@gmail.com>
2015-03-08 17:11:57 +0000

    adding routes

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(Base):
     name = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
     role = db.Column(db.String(120), unique=False)
+    isActive = db.Column(db.Boolean)
 
     def __repr__(self):
         return '<User: {}>'.format(self.email)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,65 @@
 from app import app
 from app import models
 
+
+def send_email(email, msg):
+    return
+
+
+def select_reviewers():
+    #select two reviewers
+    return ['reviewerOne', 'reviewerTwo']
+
+
 @app.route('/')
-@app.route('/index')
+@app.route('/ping')
 def index():
-    return "Hello, World!"
+    return "pong"
+
+
+@app.route('/applications/all', methods=['GET'])
+def get_applications(request):
+    return 'applications'
+
+
+@app.route('/application/update', methods=['POST'])
+def update_application(request):
+    return
+
+
+@app.route('/application/email', methods=['POST'])
+def email_applicant(request):
+    return
+
+
+@app.route('/applications/new', methods=['POST'])
+def new_application(request):
+    #Send e-mail to applicant
+    #Select Reviewers: get all reviewers for reviewer = active,
+    #sort by number of applications
+    #Email Reviewers
+    return 'applications'
+
+
+@app.route('/reviewer/new', methods=['POST'])
+def add_reviewer(request):
+    user = models.User(name=request.form['name'],
+                       email=request.form['email'],
+                       role=request.form['role'],
+                       password=)
+    user.save()
+
+    #only if user.role is admin
+    #send e-mail to new reviewer with username and password
+    return 'password'
+
+
+@app.route('/reviewer/login', methods=['POST'])
+def login(request):
+    return
+
+
+@app.route('/reviewer/logout', methods=['GET'])
+def logout(request):
+    return
+
