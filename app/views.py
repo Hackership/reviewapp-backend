a17938aaf5162c12567388fb3884c17951a8d182
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-03-13 19:27:16 +0000

    better formatting support by going through json

--- app/views.py
+++ app/views.py
@@ -173,9 +173,10 @@ def update_application():
 
 @app.route('/applications/new', methods=['POST'])
 def new_application():
-    form = request.form
-    if not form['_token'] == app.config.get("SCHEMA_TOKEN"):
+    if not request.form['_token'] == app.config.get("SCHEMA_TOKEN"):
         return jsonify(success=False)
+
+    form = json.loads(request.form['payload'])
 
     grant = 'Applying for a Programme Fee Grant' in form
 
@@ -200,9 +201,10 @@ def new_application():
     db.session.commit()
 
     # E-mail Applicant
-    application.send_email('Application Received',
-                           render_template("emails/applicant/received.md",
-                                           app=application))
+    if not request.form.get('_skip_email', False):
+        application.send_email('Application Received',
+                               render_template("emails/applicant/received.md",
+                                               app=application))
 
     return jsonify(success=True)
 
