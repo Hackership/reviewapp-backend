9296ebb1541c23caa14e8dd4ccd3bbf1fca9dc0f
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-03-23 12:05:12 -0700

    proper cleanup before deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ export current=`git rev-parse HEAD`
 
 git checkout -b $branch
 npm install
+rm -rf assets/*
+git checkout assets/
 webpack
 git add -f assets/*
 git commit -m"Add compiled UIs for deploy"
