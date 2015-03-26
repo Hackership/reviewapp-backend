f06108b2df0b3ab648c0957020a09c0ff2941440
anoukruhaak <anoukruhaak@gmail.com>
2015-03-26 12:42:51 -0700

    skype cal file added

---
+++ jsapp/scripts/components/SkypeCal.jsx
@@ -0,0 +1,16 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+
+var React = require('react/addons'),
+  _ = require('underscore'),
+	rtbs  = require('react-bootstrap'),
+  {applications} = require('../stores/ApplicationStore'),
+  {Application} = require('./ApplicationList'),
+  {user} = require('../stores/UserStore');
+
+  var SkypeCal = React.createClass({
+
+  });
