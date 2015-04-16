cd807eb5cc1197d0dddd3dcf762c5052c2748883
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-04-15 23:32:33 -0700

    add uglify to optimise the code again

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -71,7 +71,8 @@ module.exports = {
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}})
   ]
 
 };
